d52497ffc0b52ff162e3c9cdcfae6c1adbd85409
Zzhgitup <99540215+Zzhgitup@users.noreply.github.com>
2023-06-02 08:36:49 +0800

    有序二维数组查找目标值

---
+++ 手写代码篇/数据处理/Javascript版/有序二维数组查找目标值.js
@@ -0,0 +1,15 @@
+function FindNumber2DArray(matrix, target) {
+  if (matrix.length == 0 || matrix == null) return false;
+  let row = 0; //高度,
+  let column = matrix[0].length - 1; //宽度
+  while (row < matrix.length && column >= 0) {
+    if (matrix[row][column] === target) {
+      return true;
+    } else if (matrix[row][column] > target) {
+      column--;
+    } else {
+      row++;
+    }
+  }
+  return false;
+}
